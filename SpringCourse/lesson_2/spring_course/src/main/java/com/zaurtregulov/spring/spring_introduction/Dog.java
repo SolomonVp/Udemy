76c44be6415f630d1cd928cc2713dd037b31d67b
SolomonVP <Lum7023@yandex.ru>
2021-11-03 06:50:08 +0300

    adding: Java: Iterator LinkedList
    Spring:
    @Autowired
    @Qualifier

--- SpringCourse/lesson_2/spring_course/src/main/java/com/zaurtregulov/spring/spring_introduction/Dog.java
+++ SpringCourse/lesson_2/spring_course/src/main/java/com/zaurtregulov/spring/spring_introduction/Dog.java
@@ -1,8 +1,13 @@
 package com.zaurtregulov.spring.spring_introduction;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component // dog
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+//@Component // dog
+
 public class Dog implements Pet{
 //    private String name;
     public Dog() {
@@ -14,13 +19,14 @@ public class Dog implements Pet{
         System.out.println("- Гав-Гав-Гав, я твой пёсель!");
     }
 
-    public void init() {
-        System.out.println("Class Dog: init-method");
-    }
-
-    public void destroy() {
-        System.out.println("Class Dog: destroy-method");
-    }
+//    @PostConstruct
+//    public void init() {
+//        System.out.println("Class Dog: init-method");
+//    }
+//    @PreDestroy
+//    public void destroy() {
+//        System.out.println("Class Dog: destroy-method");
+//    }
 
 //    public String getName() {
 //        return name;
--- SpringCourse/lesson_2/spring_course/src/main/java/com/zaurtregulov/spring/spring_introduction/Person.java
+++ SpringCourse/lesson_2/spring_course/src/main/java/com/zaurtregulov/spring/spring_introduction/Person.java
@@ -2,18 +2,27 @@ package com.zaurtregulov.spring.spring_introduction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component("personBean")
+//@Component("personBean")
 public class Person {
-//    @Autowired
+    //    @Autowired
 //    @Qualifier("catBean")
     private Pet pet;
+    @Value("${person.surname}")
     private String surName;
+    @Value("${person.age}")
     private int age;
 
-    @Autowired
-    public Person(@Qualifier("catBean") Pet pet) {
+//    @Autowired
+//    public Person(@Qualifier("catBean") Pet pet) {
+//        System.out.println("Person bean is created");
+//        this.pet = pet;
+//    }
+
+
+    public Person(Pet pet) {
         System.out.println("Person bean is created");
         this.pet = pet;
     }
@@ -22,7 +31,7 @@ public class Person {
 //        System.out.println("Person bean is created");
 //    }
 
-//    @Autowired
+    //    @Autowired
 //    @Qualifier("dog")
     public void setPet(Pet pet) {
         System.out.println("Class Person: set Pet");
@@ -33,13 +42,16 @@ public class Person {
     public String getSurName() {
         return surName;
     }
+
     public void setSurName(String surName) {
         System.out.println("Class Person: set surname");
         this.surName = surName;
     }
+
     public int getAge() {
         return age;
     }
+
     public void setAge(int age) {
         System.out.println("Class Person: set age");
         this.age = age;
